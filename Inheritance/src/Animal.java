c38a9d518d365e28c0e67245924571eb87d903db
Hiro-1978 <78284161+Hiro-1978@users.noreply.github.com>
2021-10-30 10:18:42 +0700

    OOP EP01
    InheritanceChallenge

--- Inheritance/src/Animal.java
+++ Inheritance/src/Animal.java
@@ -16,11 +16,14 @@ public class Animal {
 
     public void eat() {
         System.out.println("Animal.eat() called");
-    }
-
-    public void move() {
 
     }
+
+    public void move(int speed) {
+        System.out.println("Animal.move() called.  Animal is moving at " +speed);
+
+    }
+
 
 
     public String getName() {
